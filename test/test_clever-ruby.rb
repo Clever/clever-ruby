1eee6148b948a5211f8d91eefadc5a45785b7c1c
Rafael Garcia <rgarcia2009@gmail.com>
2012-07-06 17:59:32 -0700

    tabs -> space

--- test/test_clever-ruby.rb
+++ test/test_clever-ruby.rb
@@ -3,7 +3,7 @@ require 'clever-ruby'
 
 class TestCleverRuby < Test::Unit::TestCase
   should "set the api key" do
-  	CleverRuby.api_key = "DEMO_KEY"
-  	assert_equal("DEMO_KEY", CleverRuby.api_key)
+    CleverRuby.api_key = "DEMO_KEY"
+    assert_equal("DEMO_KEY", CleverRuby.api_key)
   end
 end
