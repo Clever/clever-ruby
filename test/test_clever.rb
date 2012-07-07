a5bc1bd1b80203b8959417d416157f94d350e58e
Rafael Garcia <rgarcia2009@gmail.com>
2012-07-06 19:01:43 -0700

    more includes, more test

--- test/test_clever.rb
+++ test/test_clever.rb
@@ -7,8 +7,16 @@ class TestClever < Test::Unit::TestCase
     assert_equal("DEMO_KEY", Clever.api_key)
   end
 
-  should "return an api url" do
+  should "return api urls" do
     assert_equal("https://api.getclever.com/v1.1/students", Clever.api_url("students"))
+    assert_equal("https://api.getclever.com/v1.1/teachers", Clever.api_url("teachers"))
+    assert_equal("https://api.getclever.com/v1.1/schools", Clever.api_url("schools"))
+    assert_equal("https://api.getclever.com/v1.1/sections", Clever.api_url("sections"))
+  end
+
+  should "retrieves students" do
+    Clever.api_key = "DEMO_KEY"
+    Clever::Student.all()
   end
 
 end
