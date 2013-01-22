77e912c0ea3a4e3e48ca952ebd183e8612f6b1d4
Carol Nichols <cnichols@thinkthroughmath.com>
2013-01-22 15:10:21 -0500

    Add VCR for recording web requests and responses during integration tests
    This improves reliability, reproducibility, isolation, and speed of the tests. The cassettes can be rerecorded at any time if they get out of sync with the Clever service.

--- test/integration/api_operations/list_test.rb
+++ test/integration/api_operations/list_test.rb
@@ -3,33 +3,45 @@ require 'test_helper'
 class ListTest < Test::Unit::TestCase
   should "retrieves districts" do
     Clever.api_key = "DEMO_KEY"
-    Clever::District.all()
+    VCR.use_cassette("districts") do
+      Clever::District.all()
+    end
   end
 
   should "retrieves schools" do
     Clever.api_key = "DEMO_KEY"
-    Clever::School.all()
+    VCR.use_cassette("schools") do
+      Clever::School.all()
+    end
   end
 
   should "retrieves students" do
     Clever.api_key = "DEMO_KEY"
-    students = Clever::Student.all()
-    student = students[0]
+    VCR.use_cassette("students") do
+      @students = Clever::Student.all()
+    end
+
+    student = @students[0]
   end
 
   should "retrieves sections" do
     Clever.api_key = "DEMO_KEY"
-    Clever::Section.all()
+    VCR.use_cassette("sections") do
+      Clever::Section.all()
+    end
   end
 
   should "retrieves teachers" do
     Clever.api_key = "DEMO_KEY"
-    teachers = Clever::Teacher.all()
-    for teacher in teachers
-      teacher_obj = Clever::Teacher.retrieve teacher.id
-      assert_instance_of(Clever::Teacher, teacher)
-      assert_instance_of(Clever::Teacher, teacher_obj)
-      assert_equal(teacher, teacher_obj)
+    VCR.use_cassette("teachers") do
+      @teachers = Clever::Teacher.all()
+
+      for teacher in @teachers
+        teacher_obj = Clever::Teacher.retrieve teacher.id
+        assert_instance_of(Clever::Teacher, teacher)
+        assert_instance_of(Clever::Teacher, teacher_obj)
+        assert_equal(teacher, teacher_obj)
+      end
     end
   end
 end
