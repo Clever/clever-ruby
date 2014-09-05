66eba82b89b444a7ad7d986e86499b9f43f38fe8
Omar Diab <omar.diab@clever.com>
2014-09-05 16:20:50 -0700

    make Page enumerable, make hypermedia links accessible

--- lib/clever-ruby/api_operations/page.rb
+++ lib/clever-ruby/api_operations/page.rb
@@ -2,7 +2,10 @@ module Clever
   module APIOperations
     # Represents a page of data
     class Page
+      include Enumerable
+
       attr_accessor :paging
+      attr_accessor :links
 
       def initialize(uri, filters = {})
         @uri = uri
@@ -11,11 +14,23 @@ module Clever
         response = Clever.request :get, uri, filters
         @list = Util.convert_to_clever_object response[:data]
         self.paging = response[:paging]
+        self.links = {}
+        response[:links].each do |link|
+          links[link[:rel].to_sym] = link[:uri]
+        end
       end
 
-      # rubocop:disable TrivialAccessors
+      def each(&blk)
+        @list.each(&blk)
+      end
+
+      # TODO: remove this
       def all
-        @list
+        accum = []
+        each do |elem|
+          accum << elem
+        end
+        accum
       end
     end
   end
