2b9a4e2546f35e503eaaa759bb44e01a8adb09ee
Omar Diab <omar.diab@clever.com>
2014-09-10 15:02:17 -0700

    remove photo method in students

--- lib/clever-ruby/student.rb
+++ lib/clever-ruby/student.rb
@@ -8,17 +8,5 @@ module Clever
       [:student_number, :state_id, :location, :gender, :dob, :grade, :frl_status,
        :race, :hispanic_ethnicity, :email, :credentials]
     end
-
-    def photo
-      return @values[:photo] if @values.key? :photo
-      response = Clever.request :get, photo_url
-      @values[:photo] = response[:data][:data]
-    end
-
-    private
-
-    def photo_url
-      url + '/photo'
-    end
   end
 end
