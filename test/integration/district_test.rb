d724f02b7c220f9f7c4fc66f5525a56d8cf52bb2
Omar Diab <omar.diab@clever.com>
2014-09-12 13:54:50 -0700

    add tests for nested resources

--- test/integration/district_test.rb
+++ test/integration/district_test.rb
@@ -3,14 +3,6 @@ require 'test_helper'
 module Minitest
   # Add assertions to Minitest
   module Assertions
-    def assert_valid_object_list(plural_object_name, instance_name)
-      VCR.use_cassette("districts_#{plural_object_name}", allow_playback_repeats: true) do
-        district = Clever::District.all.first
-        district.send(plural_object_name).size.must_equal district.send(plural_object_name).count
-        district.send(plural_object_name).first.must_be_instance_of instance_name
-      end
-    end
-
     def assert_valid_object_pages(object_name, limit, page_count)
       VCR.use_cassette("districts_#{object_name}_pages", allow_playback_repeats: true) do
         district = Clever::District.all.first
@@ -34,47 +26,30 @@ end
 describe Clever::District do
   before do
     Clever.configure do |config|
-      config.api_key = 'DEMO_KEY'
+      config.token = 'DEMO_TOKEN'
     end
   end
 
-  it "retrieves a district's schools" do
-    assert_valid_object_list 'schools', Clever::School
-  end
+  # TODO: this is outdated and should be removed!
+  describe 'pages methods' do
+    it "pages a district's schools" do
+      assert_valid_object_pages 'school', 2, 2
+    end
 
-  it "retrieves a district's teachers" do
-    assert_valid_object_list 'teachers', Clever::Teacher
-  end
+    it "pages a district's teachers" do
+      assert_valid_object_pages 'teacher', 10, 9
+    end
 
-  it "retrieves a district's sections" do
-    assert_valid_object_list 'sections', Clever::Section
-  end
+    it "pages a district's sections" do
+      assert_valid_object_pages 'section', 10, 38
+    end
 
-  it "retrieves a district's students" do
-    assert_valid_object_list 'students', Clever::Student
-  end
+    it "pages a district's students" do
+      assert_valid_object_pages 'student', 50, 21
+    end
 
-  it "retrieves a district's events" do
-    assert_valid_object_list 'events', Clever::Event
-  end
-
-  it "pages a district's schools" do
-    assert_valid_object_pages 'school', 2, 2
-  end
-
-  it "pages a district's teachers" do
-    assert_valid_object_pages 'teacher', 10, 9
-  end
-
-  it "pages a district's sections" do
-    assert_valid_object_pages 'section', 10, 38
-  end
-
-  it "pages a district's students" do
-    assert_valid_object_pages 'student', 50, 21
-  end
-
-  it "pages a district's events" do
-    assert_valid_object_pages 'event', 1, 8
+    it "pages a district's events" do
+      assert_valid_object_pages 'event', 1, 8
+    end
   end
 end
