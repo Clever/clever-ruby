8ccf0699ab6afbfdbc87489a1898a88d69b7f9b4
Carol Nichols <carol.nichols@gmail.com>
2015-09-22 17:09:20 -0400

    Add objects for school admins and a method on district to get them
    The district response does not have links to the school_admins URL,
    so we need to hard code it instead of using @linked_resources.

---
+++ lib/clever-ruby/school_admin.rb
@@ -0,0 +1,16 @@
+module Clever
+  class SchoolAdmin < APIResource
+    include Clever::APIOperations::List
+    @uri = 'school_admins'
+    @plural = 'school_admins'
+    @linked_resources = [:schools]
+
+    # Optional attributes
+    # @see Clever::CleverObject.optional_attributes
+    # @api private
+    # @return [Array]
+    def optional_attributes
+      [:email, :title, :staff_id]
+    end
+  end
+end
