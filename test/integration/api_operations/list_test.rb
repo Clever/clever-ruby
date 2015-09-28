43bc5f4516506d54cd12f1aabb7d307a73da9a53
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2015-09-28 12:33:23 -0400

    Remove District Admin from API Operations list tests
    The district admin resources don't support the list parameters.

--- test/integration/api_operations/list_test.rb
+++ test/integration/api_operations/list_test.rb
@@ -7,7 +7,7 @@ end
 def resources
   [Clever::District, Clever::School, Clever::Teacher,
    Clever::Student, Clever::Section, Clever::Event,
-   Clever::DistrictAdmin, Clever::SchoolAdmin]
+   Clever::SchoolAdmin]
 end
 
 module Minitest
@@ -30,7 +30,7 @@ describe Clever::APIOperations::List, :vcr do
 
   [[Clever::District, 1], [Clever::School, 3], [Clever::Teacher, 89],
    [Clever::Student, 1004], [Clever::Section, 379], [Clever::Event, 7],
-   [Clever::DistrictAdmin, 1], [Clever::SchoolAdmin, 1]].each do |test_data|
+   [Clever::SchoolAdmin, 1]].each do |test_data|
     resource, expected = test_data
     name = resource_name resource
 
