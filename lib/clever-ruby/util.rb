bc53fa0ee06cca2f71535615c099f7757a199599
George Lin <george@zearn.com>
2016-12-15 18:35:23 -0500

    Ensure DistrictAdmin resource can be be constructed from API response based on URI
    When using Clever::DistrictAdmin.find directly, the URI may not contain the first
    forward slash, so we ignore that when trying to extract the klass_name

--- lib/clever-ruby/util.rb
+++ lib/clever-ruby/util.rb
@@ -43,7 +43,7 @@ module Clever
           uri = resp[:links].select { |l| l[:rel] == 'self' }[0][:uri]
         end
 
-        klass_name = %r{/v1.1/([a-z_]+)/\S+$}.match(uri)[1]
+        klass_name = %r{v1.1/([a-z_]+)/\S+$}.match(uri)[1]
         klass = APIResource.named klass_name if klass_name
         klass ||= CleverObject
         klass.construct_from resp[:data]
