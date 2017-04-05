8b01ca019666de63ae588f883675e82a08220b44
Kyle Vigen <kyle.vigen@clever.com>
2017-04-05 11:50:02 -0700

    Code review feedback

--- sample/sample.rb
+++ sample/sample.rb
@@ -9,13 +9,7 @@ end
 api_instance = CleverAPI::DefaultApi.new
 
 begin
-  #students = api_instance.get_students()
-  #p students
-
-  event = api_instance.get_event("58c854e71e23b6f39850be27")
-  p "Event", event
-
-  events = api_instance.get_events({:limit => 5})
-  puts "Events ", events.data
+  students = api_instance.get_students()
+  p students
 
 end
