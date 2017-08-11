af2e6f1da17791a85a228e704a6eddb840fecb4c
ReviakinAleksey <it.blackdog@gmail.com>
2017-08-11 13:59:35 +0300

    Test sample for demonstration error on School high_grade empty value

--- spec/events_api_spec.rb
+++ spec/events_api_spec.rb
@@ -8,13 +8,15 @@ describe Clever::EventsApi do
     Clever.configure do |config|
       config.access_token = 'DEMO_TOKEN'
     end
-
-    file = File.read('spec/events.json')
-    stub_request(:get, /api.clever.com/).
-      to_return(status: 200, body: file, headers: {})
   end
 
   describe ".get_events" do
+    before(:each) do
+      file = File.read('spec/events.json')
+      stub_request(:get, /api.clever.com/).
+          to_return(status: 200, body: file, headers: {})
+    end
+
     context "given events exist" do
       it "does not raise an error" do
         expect{@api_instance.get_events}.to_not raise_error
@@ -30,4 +32,19 @@ describe Clever::EventsApi do
       end
     end
   end
+
+  describe ".get_empty_grade_event" do
+    before(:each) do
+      file = File.read('spec/empty_grade_fields.json')
+      stub_request(:get, /api.clever.com/).
+          to_return(status: 200, body: file, headers: {})
+    end
+
+    context "empty grade events exist" do
+      it "does not raise an error" do
+        expect {@api_instance.get_events}.to_not raise_error
+      end
+    end
+  end
+
 end
