b5ea3a91c30fd7b87d5676e2154abdeac0b367cc
Chris Salvato <csalvato@gmail.com>
2022-04-13 11:38:14 -0600

    Changes escape method to support ruby 3

--- lib/clever-ruby/configuration.rb
+++ lib/clever-ruby/configuration.rb
@@ -175,7 +175,7 @@ module Clever
 
     def base_url
       url = "#{scheme}://#{[host, base_path].join('/').gsub(/\/+/, '/')}".sub(/\/+\z/, '')
-      URI.encode(url)
+      Addressable::URI.parse(url).normalize.to_s
     end
 
     # Gets API key (with prefix if set).
